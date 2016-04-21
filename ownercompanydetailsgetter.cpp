bf216752f5633d0f545f83e0cf77de3a429f6ed4
Gadheyan Sivakaran <gadheyansivakaran@gmail.com>
2016-04-21 09:53:12 +0530

    Update ownercompanydetailsgetter.cpp

--- ownercompanydetailsgetter.cpp
+++ ownercompanydetailsgetter.cpp
@@ -1,7 +1,10 @@
 #include "ownercompanydetailsgetter.h"
 #include "ui_ownercompanydetailsgetter.h"
 #include "dbconnector.h"
-#include <QMessageBox>
+#include "QMessageBox"
+#include <QFileDialog>
+#include <QRegularExpression>
+
 
 ownerCompanyDetailsGetter::ownerCompanyDetailsGetter(QWidget *parent) :
     QDialog(parent),
@@ -18,23 +21,48 @@ ownerCompanyDetailsGetter::~ownerCompanyDetailsGetter()
 
 void ownerCompanyDetailsGetter::on_pushButton_clicked()
 {
-    QString owner_name,company_name,address,email;
-    owner_name=ui->owner_name->text();
-    company_name=ui->company_name->text();
-    address=ui->address->toPlainText();
-    email=ui->email->text();
+
     dbconnector db;
-    db.insertIntoOwner(owner_name,company_name,email,address);
+    QString company_name,email,address,additional_info;
+    company_name = ui->company_name->text();
+    email = ui->email->text();
+    address = ui->address->toPlainText();
+    additional_info = ui->additional_info->text();
+    db.insertIntoOwner(company_name,email,address,additional_info);
     QMessageBox::information(
         this,
         tr("Saved"),
         tr("Information has been saved.") );
+
+
+
+
+
     this->close();
+
 }
 
 void ownerCompanyDetailsGetter::on_pushButton_2_clicked()
 {
     this->close();
+}
+
+void ownerCompanyDetailsGetter::on_email_textChanged(const QString &arg)
+{
+    QString email;
+    email = ui->email->text();
+
+    QRegularExpression regex("^[0-9a-zA-Z]+([0-9a-zA-Z]*[-._+])*[0-9a-zA-Z]+@[0-9a-zA-Z]+([-.][0-9a-zA-Z]+)*([0-9a-zA-Z]*[.])[a-zA-Z]{2,6}$");
+    if(!regex.match(email).hasMatch())
+        {
+            ui->email_validation->setStyleSheet("QLabel {  color : red; }");
+            ui->email_validation->setText("Not valid");
+        }
+    else
+    {
+        ui->email_validation->setStyleSheet("QLabel {  color : green; }");
+        ui->email_validation->setText("valid");
+    }
 
 
 }
